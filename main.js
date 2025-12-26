78ea7eb1e6b4b847b46c165954c77db6332f67b1
avfp <pau.avilesfilppu@gmail.com>
2025-12-26 19:46:21 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ProjectMargo from './ProjectMargo.js';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(React.createElement(ProjectMargo));
